6651163bedc8d072b63cd1b3d201ee2fb7b1681b
yin0625 <65537640@qq.com>
2021-01-26 10:41:43 +0800

    修改端口号

--- src/main/java/com/example/jenkins/JenkinsApplication.java
+++ src/main/java/com/example/jenkins/JenkinsApplication.java
@@ -2,12 +2,18 @@ package com.example.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class JenkinsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String index(){
+		return "jenkins start success";
 	}
 
 }
