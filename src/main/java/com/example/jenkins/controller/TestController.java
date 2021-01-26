a5865c9b24d79e4148735074705994a8997aec1c
yin0625 <65537640@qq.com>
2021-01-26 10:54:24 +0800

    修改端口号

--- src/main/java/com/example/jenkins/controller/TestController.java
+++ src/main/java/com/example/jenkins/controller/TestController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class TestController {
 
-    @RequestMapping("/")
+    @RequestMapping
     public String index(){
         return "jenkins start success";
     }
