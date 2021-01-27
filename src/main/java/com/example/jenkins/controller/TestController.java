3a81d76e5d48fadfd4081d21f19e4e3183b9bf14
yin0625 <65537640@qq.com>
2021-01-27 11:12:13 +0800

    增加lombok日志

--- src/main/java/com/example/jenkins/controller/TestController.java
+++ src/main/java/com/example/jenkins/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.example.jenkins.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,17 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
  * @author yinshu
  * @since 2021/1/18 15:55
  */
+@Slf4j
 @RestController
 @RequestMapping
 public class TestController {
 
     @RequestMapping
     public String index(){
+        log.info("jenkins start success");
         return "jenkins start success";
     }
 
     @RequestMapping("/test")
     public String test(){
+        log.info("jenkins test");
         return "jenkins test";
     }
 }
